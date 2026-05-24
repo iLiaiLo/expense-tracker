af6a0bed76f804f2d70093d366074e0008c0fef9
Ilo Tech <ilo.nikatsadze@gmail.com>
2026-05-24 18:35:02 +0400

    message updated

--- services/sendDeleteEmailStatus.js
+++ services/sendDeleteEmailStatus.js
@@ -8,11 +8,11 @@ const sendDeleteEmailStatus = async (req, res, next) => {
       from: process.env.SENDER_EMAIL,
       to: email,
       subject: `for ${email}`,
-      text: "Your account has been deleted. you can recover it in 10 minutes or it will be deleted permanently.",
+      text: "Your account has been deleted. you can recover it in 30 days or it will be deleted permanently.",
     });
     return res.status(200).json({
       message:
-        "Your account has been deleted. you can recover it in 10 minutes or it will be deleted permanently.",
+        "Your account has been deleted. you can recover it in 30 days or it will be deleted permanently.",
     });
   } catch (error) {
     next(error);

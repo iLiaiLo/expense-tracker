6c79d0dce4bcaaf62a65ad2a2ecfdd82d7d79c1f
Ilo Tech <ilo.nikatsadze@gmail.com>
2026-05-24 17:43:35 +0400

    cleanup service created

---
+++ cron/startCleanupService.js
@@ -0,0 +1,17 @@
+import cron from "node-cron";
+import { pool } from "../db/connectoin.js";
+import getQuery from "../utils/getQuery.js";
+const startCleanupService = () => {
+  cron.schedule("*/3 * * * *", async () => {
+    try {
+      //three minutes for testing purposes
+      console.log("Running scheduled cleanup...");
+      const deleteQuery = getQuery("user_queries/HARD_DELETE_ACCOUNT.sql");
+      const result = await pool.query(deleteQuery);
+      console.log(`Purged ${result.rowCount} accounts.`);
+    } catch (error) {
+      console.error("Cleanup Task Failed:", error);
+    }
+  });
+};
+export default startCleanupService;

dda59fdbd9cf44258e2d40da6084ee7e69f45615
Ilo Tech <ilo.nikatsadze@gmail.com>
2026-07-14 21:51:41 +0400

    import raw query string directly

--- cron/startCleanupService.js
+++ cron/startCleanupService.js
@@ -1,12 +1,12 @@
 import cron from "node-cron";
 import { pool } from "../db/connectoin.js";
-import getQuery from "../utils/getQuery.js";
+import { hardDeleteQuery } from "../queries/user_queries/HARD_DELETE_ACCOUNT.js";
 const startCleanupService = () => {
   cron.schedule("0 0 * * *", async () => {
     try {
       console.log("Running scheduled cleanup...");
       const deleteQuery = getQuery("user_queries/HARD_DELETE_ACCOUNT.sql");
-      const result = await pool.query(deleteQuery);
+      const result = await pool.query(hardDeleteQuery);
       console.log(`Purged ${result.rowCount} accounts.`);
     } catch (error) {
       console.error("Cleanup Task Failed:", error);
